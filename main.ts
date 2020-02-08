dacce8882f230a05001d9d424f19102634d04700
Loochs <54494160+loochs@users.noreply.github.com>
2020-02-08 21:56:27 +0900

    [add] first step to learn

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.AB, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    basic.pause(500)
+    maqueen.motorStop(maqueen.Motors.All)
+})
+input.onButtonPressed(Button.B, function () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    basic.pause(500)
+    maqueen.motorStop(maqueen.Motors.M2)
+})
+input.onButtonPressed(Button.A, function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    basic.pause(500)
+    maqueen.motorStop(maqueen.Motors.M1)
+})
 basic.forever(function () {
 	
 })
